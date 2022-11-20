a8912fcddad5cb160628c69c2ca2288b20fba992
yancarlodev <yancarlodc@gmail.com>
2022-11-19 21:08:20 -0300

    feat: allow cors to make web request

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,12 @@ import { PrismaClient } from '@prisma/client'
 import handleErrorsMiddleware from './middlewares/handleErrors.middleware'
 import sessionRouter from './routes/session.routes'
 import transactionRouter from './routes/transaction.routes'
-
+import cors from 'cors'
 
 export const prisma = new PrismaClient()
 export const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/session', sessionRouter)
 app.use('/transaction', transactionRouter)
